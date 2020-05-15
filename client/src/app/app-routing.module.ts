a871d34b390366e5912cc69985d3e834546e4c4c
unknown <nouhachorfi2@gmail.com>
2020-05-15 17:27:49 +0100

    courses routing + fetching videos

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CoursesComponent } from './courses/courses.component';
+import { CourseComponent } from './course/course.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'courses', component: CoursesComponent },
+  {
+    path: 'courses',
+    component: CoursesComponent,
+  },
+  {
+    path: 'courses/:id',
+    component: CourseComponent,
+    // children: [{ path: ':id', component: CourseComponent }],
+  },
 ];
 
 @NgModule({
--- client/src/app/courses/courses.component.ts
+++ client/src/app/courses/courses.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router, ActivatedRoute } from '@angular/router';
 
 const baseUrl = 'http://localhost:8080/courses';
-
 // @Injectable({
 //   providedIn: 'root',
 // })
@@ -12,14 +12,39 @@ const baseUrl = 'http://localhost:8080/courses';
   styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
+  idCourse;
   courses = <any>[];
-  constructor(private http: HttpClient) {
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private activatedRote: ActivatedRoute
+  ) {
     console.log('Called Constructor');
   }
 
   getAllCourses() {
     let data = this.http.get<any>(baseUrl);
     return data;
+  }
+
+  getID(id) {
+    return id;
+  }
+
+  public goToCourse(url, id) {
+    this.router.navigate(['courses', id]);
+
+    // // course/1
+    // var myUrl = `${url}/${id}`;
+    // this.idCourse = id;
+    // this.getID(this.idCourse);
+    // this.router.navigateByUrl(myUrl).then((e) => {
+    //   if (e) {
+    //     console.log('Navigation is successful!');
+    //   } else {
+    //     console.log('Navigation has failed!');
+    //   }
+    // });
   }
   ngOnInit(): void {
     console.log('Called ONINIT');
